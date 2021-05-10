ed56180a6c9209c42a7fbe4eb434d61762de48cf
Abdullahi Muhammad <muhammedabduls20@gmail.com>
2021-05-10 01:55:51 +0100

    store and carts functionality

--- carts/urls.py
+++ carts/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
 
     path('', views.cart, name="cart"),
     path('add_cart/<int:product_id>/', views.add_cart, name="add_cart"),
-
+    path('reduce_cart/<int:product_id>/<int:cart_item_id>/', views.reduce_cart, name="reduce_cart"),
+    path('delete_cart/<int:product_id>/<int:cart_item_id>/', views.delete_cart, name="delete_cart"),
 ]
