e240a204ed4905bc6b60b99de3af42cb41d5618e
Abdullahi Muhammad <muhammedabduls20@gmail.com>
2021-05-17 19:22:33 +0100

    handled login and check out functionlality

--- carts/urls.py
+++ carts/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('add_cart/<int:product_id>/', views.add_cart, name="add_cart"),
     path('reduce_cart/<int:product_id>/<int:cart_item_id>/', views.reduce_cart, name="reduce_cart"),
     path('delete_cart/<int:product_id>/<int:cart_item_id>/', views.delete_cart, name="delete_cart"),
+
+    path('checkout/', views.checkout, name='checkout'),
 ]
